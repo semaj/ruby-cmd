fc26032d4f320c3aa327cddda796f898b6c05389
James Larisch <root@jameslarisch.com>
2014-07-31 23:31:32 -0400

    some better notation and comments in mining sim

--- btc-mining-sim.rb
+++ btc-mining-sim.rb
@@ -16,7 +16,7 @@ def hex_bytestr_to_hex_str(bytestr)
   bytestr.unpack('H*').first
 end
 
-def first_2_bytes(bytes)
+def first_byte(bytes)
   bytes.to_s(16)[0..1].hex # AKA `bytes >> (4 * ((# OF BYTES) - 2))`
 end
 
@@ -31,14 +31,15 @@ end
 version = 2
 prev_block = "000000000000000117c80378b8da0e33559b5997f2ad55e2f7d18ec1975b9717"
 merkle_root = "871714dcbae6c8193a2bb9b2a69fe1c0440399f38d94b3a0f1b447275a29978a"
-time = 0x53058b35 # 2014-02-20 04:57:25
+time = 1392872245 # 2014-02-20 04:57:25
 bits = 0x19015f53
  
 # https://en.bitcoin.it/wiki/Difficulty
 target_hexstr = zero_padded_256bit_str(
-  rest_bytes(bits) * (2 ** (8 * (first_2_bytes(bits) - 3))) # arbitrary formula
+  rest_bytes(bits) * (2 ** (8 * (first_byte(bits) - 3))) # arbitrary formula
 ) 
 target_bytestr = hex_str_to_hex_bytestr(target_hexstr)
+puts target_hexstr
 
 (0x0..0x1000).each do |nonce|
   header = little_endian_bytestr(version) +
@@ -48,7 +49,7 @@ target_bytestr = hex_str_to_hex_bytestr(target_hexstr)
 
   hash = sha256(sha256(header))
   reversed_hash = hash.reverse
-  puts nonce.to_s + ' ' + hex_bytestr_to_hex_str(reversed_hash)
+  #puts nonce.to_s + ' ' + hex_bytestr_to_hex_str(reversed_hash)
   if reversed_hash < target_bytestr
     puts 'SUCCESS'
   end
